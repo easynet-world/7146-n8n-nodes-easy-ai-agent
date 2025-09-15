fa174fb395ed78b491e87f00b61e1204cd14209f
Boqiang Liang <boqiang@openai.com>
2025-09-14 22:55:58 -0700

    fix: copy src directory to dist during build to resolve module import
    - Update gulpfile.js to copy src directory to dist/src during build
    - Fix import paths to use relative paths ../../src/ instead of absolute
    - This ensures the n8n node can find the required source files when installed
    - The src directory is now available in the published package at dist/src/

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
-const { src, dest } = require('gulp');
+const { src, dest, series } = require('gulp');
 
 function buildIcons() {
 	return src('nodes/**/*.{png,svg}')
 		.pipe(dest('dist/nodes'));
 }
 
+function copySrc() {
+	return src('src/**/*')
+		.pipe(dest('dist/src'));
+}
+
 exports['build:icons'] = buildIcons;
+exports['copy:src'] = copySrc;
+exports['build'] = series(buildIcons, copySrc);
